4e36e1fdd042922cf3d65092413f5f1f3d7a26c0
Andika Elang D <andika.elang2511@gmail.com>
2025-12-05 15:40:27 +0700

    feat: initial migration
    - order
    - menu
    - order detail

---
+++ database/migrations/2025_12_04_154133_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('orders', function (Blueprint $table) {
+      $table->id();
+      $table->date('order_date');
+      $table->string('buyer_name');
+      $table->enum('buyer_type', ['IT', 'NON-IT']);
+      $table->enum('payment_type', ['Cash', 'QRIS', 'Transfer']);
+      $table->boolean('is_paid')->default(false);
+      $table->boolean('extra_cup')->default(false);
+      $table->decimal('total_amount', 10, 2);
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('orders');
+  }
+};
